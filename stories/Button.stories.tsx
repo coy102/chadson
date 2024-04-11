a28eded912e70ec0b0dbd0c67cedc7a49c8b7bc5
Fajar Septiawan <fajar.septiawan@xapiens.id>
2024-04-11 20:22:13 +0700

    feat: init shadcn

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,11 +1,11 @@
 import { Meta, StoryFn } from '@storybook/react'
 import React from 'react'
 
-import { Props, Thing } from '../src'
+import { Button, ButtonProps } from '../src/components/ui/button'
 
 const meta: Meta = {
-  title: 'Welcome',
-  component: Thing,
+  title: 'Button',
+  component: Button,
   argTypes: {
     children: {
       control: {
@@ -20,7 +20,11 @@ const meta: Meta = {
 
 export default meta
 
-const Template: StoryFn<Props> = args => <Thing {...args} />
+const Template: StoryFn<ButtonProps> = args => (
+  <Button variant="destructive" {...args}>
+    Button
+  </Button>
+)
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
