38fa705f4daa5c3736a1b887253ad6a13d1c2dbc
Fajar Septiawan <fajar.septiawan@xapiens.id>
2024-04-13 18:25:48 +0700

    fix: tsdx config

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,14 +1,20 @@
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
 const postcss = require('rollup-plugin-postcss')
 
 module.exports = {
   rollup(config, options) {
     config.plugins.push(
       postcss({
+        plugins: [
+          autoprefixer(),
+          cssnano({
+            preset: 'default',
+          }),
+        ],
         config: {
           path: './postcss.config.js',
         },
-        extensions: ['.css'],
-        minimize: true,
         inject: true,
         // only write out CSS for the first bundle (avoids pointless extra files):
         extract: !!options.writeMeta,
