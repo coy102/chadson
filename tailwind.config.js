b0dbcba86e5de5a36064d484afda78e72b44f04c
Fajar Septiawan <fajar.septiawan@xapiens.id>
2024-04-14 10:18:06 +0700

    feat: add content tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,6 @@ const { withChadson } = require('./src/libs/withChadson')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = withChadson({
-  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}', './node_modules/chadson/**/*.{js,ts,jsx,tsx}'],
   plugins: [require('tailwindcss-animate')],
 })
